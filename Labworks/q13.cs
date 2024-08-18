04949b5770ce7e909c1b16fd3f98975013774563
Rosa Prajapati <160918682+rosa1soto@users.noreply.github.com>
2024-08-18 18:30:38 +0545

    Add files via upload

---
+++ Labworks/q13.cs
@@ -0,0 +1,26 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labworks
+{
+    internal class q13
+    {
+        static void Main()
+        {
+
+            string[] stringArray = { "Hello", "world", "this", "is", "C#" };
+            string result = ConvertArrayToString(stringArray, " ");
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+
+        static string ConvertArrayToString(string[] array, string delimiter)
+        {
+            return string.Join(delimiter, array);
+        }
+    }
+}
+*/
